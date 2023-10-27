24553b1a810e8a943187a58d99bcfd4588e1b031
game.desktop <game.desktop@example.com>
2023-10-27 16:29:08 +0800

    支持端口和默认页面

--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net"
 	"os"
+	"os/exec"
+	"os/signal"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
+const defaultPort = 8000
+
 func main() {
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGKILL)
+
+	_ = initIndex()
 	r := gin.Default()
 	r.Static("/", appDirectory())
-	_ = r.Run(fmt.Sprintf(":%d", portUse(8000)))
+
+	port := portUse(parseArgPort())
+
+	go func() {
+		_ = r.Run(fmt.Sprintf("127.0.0.1:%d", port))
+	}()
+	go func() {
+		cmd := exec.Command("cmd", "/c", "start", fmt.Sprintf("http://127.0.0.1:%d", port))
+		_ = cmd.Run()
+	}()
+
+	select {
+	case _sig := <-sig:
+		log.Println(fmt.Sprintf("[stop] %v\n", _sig))
+	}
+
+}
+
+func parseArgPort() int {
+	var port = defaultPort
+	fp := flag.Int("p", port, "http端口号")
+	fport := flag.Int("port", port, "http端口号")
+	flag.Parse()
+
+	if len(os.Args) > 1 {
+		port, _ = strconv.Atoi(os.Args[1])
+	}
+	if *fp != defaultPort && *fp > 0 {
+		port = *fp
+	}
+	if *fport != defaultPort && *fport > 0 {
+		port = *fport
+	}
+	return port
 }
 
 func appDirectory() string {
@@ -30,3 +77,61 @@ func portUse(port int) int {
 	defer func() { _ = listener.Close() }()
 	return port
 }
+
+func initIndex() error {
+	f := filepath.Join(appDirectory(), "index.html")
+	fi, err := os.OpenFile(f, os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	_, err = fi.WriteString(strings.ReplaceAll(htmlTemplate, "__WEB_PATH__", appDirectory()))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+const htmlTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style type="text/css">
+        html,body,.c{height: 100%;}
+        .c{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            font-family: "Microsoft JhengHei UI";
+            text-align: center;
+        }
+        .c .title{
+            font-size: 86px;
+        }
+        .c .info{
+            color: #6e6e6e;
+            margin-top: 20px;
+            font-size: 38px;
+        }
+        .c .info  span{
+            border-bottom: 2px solid #a1a1a1;
+            padding: 2px;
+            cursor: copy;
+        }
+    </style>
+</head>
+<body>
+
+<div class="c">
+    <div>
+        <div class="title">Hello, 你好</div>
+        <div class="info">file:///<span>__WEB_PATH__</span></div>
+    </div>
+</div>
+
+</body>
+</html>
+
+`
